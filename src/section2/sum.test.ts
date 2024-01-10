325191e2c6316f740a7c8426ffc87ecb2e167c50
Yoshihiro Sasago <sasayoshi.0605@gmail.com>
2024-01-10 23:23:22 +0900

    パラメタイズドテスト

--- src/section2/sum.test.ts
+++ src/section2/sum.test.ts
@@ -1,6 +1,18 @@
 import { sum } from "./sum";
 
-// toBe
-it("１と２を足すと３になる", () => {
-  expect(sum(1, 2)).toBe(3);
+// // toBe
+// it("１と２を足すと３になる", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+// it("１と-10を足すと-9になる", () => {
+//   expect(sum(1, -10)).toBe(-9);
+// });
+
+it.each`
+  a    | b      | expected
+  ${1} | ${2}   | ${3}
+  ${1} | ${-10} | ${-9}
+`("$aと$bを足すと$expectedになる", ({ a, b, expected }) => {
+  expect(sum(a, b)).toBe(expected);
 });
